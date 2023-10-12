866e64a27acb9fb29918f44755c6485e58d65857
root <root@pritunl-nl.dumasti.com>
2023-10-12 18:32:06 +0300

    Trial period ends on 12/11/2023

--- additional/connect.sh
+++ additional/connect.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#message="$(echo -e "${common_name},${untrusted_ip},${untrusted_port},${ifconfig_pool_remote_ip},${time_ascii},${bytes_received}0,${bytes_sent}0,${IV_PLAT},${IV_GUI_VER},${IV_HWADDR},connected ")"
 message="`date +'%F %T'`,$(echo -e "${common_name},${untrusted_ip},${untrusted_port},${ifconfig_pool_remote_ip},${bytes_received}0,${bytes_sent}0,${IV_PLAT},${IV_GUI_VER},${IV_HWADDR},connected ")"
 month=`date | cut -d ' ' -f 2`
 
-#dir=/var/log/logconnection
-dir=/root/logconnection
+dir=/var/log/logconnection/$name
 if [ -d "$dir" ]
     then
     :
     else
-    mkdir $dir
+    mkdir -p $dir
 fi
 
 if [ -f $dir/logs.$month ]
