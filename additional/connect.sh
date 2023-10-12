e3932c008ac0b32c1a9f61eaec7e8e9909ee71f3
dumasti <51967779+dumasti@users.noreply.github.com>
2023-10-12 17:13:14 +0300

    Update connect.sh

--- additional/connect.sh
+++ additional/connect.sh
@@ -3,7 +3,7 @@
 message="`date +'%F %T'`,$(echo -e "${common_name},${untrusted_ip},${untrusted_port},${ifconfig_pool_remote_ip},${bytes_received}0,${bytes_sent}0,${IV_PLAT},${IV_GUI_VER},${IV_HWADDR},connected ")"
 month=`date | cut -d ' ' -f 2`
 
-dir=/var/log/logconnection
+dir=/var/log/logconnection/$name
 if [ -d "$dir" ]
     then
     :
