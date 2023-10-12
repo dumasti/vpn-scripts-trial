425854f2906cee9323d62b877934a1f936a7c034
dumasti <51967779+dumasti@users.noreply.github.com>
2023-10-12 16:19:46 +0300

    Update disconnect.sh
    Change path for logs

--- additional/disconnect.sh
+++ additional/disconnect.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#message="$(echo -e "${common_name},${untrusted_ip},${untrusted_port},${ifconfig_pool_remote_ip},${time_ascii},${bytes_received},${bytes_sent},${IV_PLAT},${IV_GUI_VER},${IV_HWADDR},disconnected")"
 message="`date +'%F %T'`,$(echo -e "${common_name},${untrusted_ip},${untrusted_port},${ifconfig_pool_remote_ip},${bytes_received},${bytes_sent},${IV_PLAT},${IV_GUI_VER},${IV_HWADDR},disconnected")"
 month=`date | cut -d ' ' -f 2`
 
-#dir=/var/log/logconnection
-dir=/root/logconnection
+dir=/var/log/logconnection
 if [ -d "$dir" ]
     then
     :
@@ -20,7 +18,5 @@ if [ -f $dir/logs.$month ]
         touch $dir/logs.$month
         echo "${message}" >> $dir/logs.$month
 fi
-#${time_duration}
-#${IV_PLAT_VER}
 
 exit 0
